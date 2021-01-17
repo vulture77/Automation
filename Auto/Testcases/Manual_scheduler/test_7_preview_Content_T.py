8488cc3a725d842afcac27125bb8d247e5ddc1d2
vulture77 <60322348+vulture77@users.noreply.github.com>
2021-01-17 21:58:45 +0530

    Add files via upload

---
+++ Auto/Testcases/Manual_scheduler/test_7_preview_Content_T.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+import unittest
+import random
+import string
+import time
+from PrabandhakPages.Manualscheduler import ManualSchedulermixin
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")
+
+
+class login(unittest.TestCase,ManualSchedulermixin):
+
+    qa_username = 'admin@reverieinc.com'
+    qa_password = 'admin'
+
+    randstring = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+   
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.driver = webdriver.Chrome(executable_path="C:\\Users\\taan\\Desktop\\Auto\\Drivers\\chrome\\chromedriver.exe", options=chrome_options)
+        #cls.driver = webdriver.Chrome(executable_path="C:\\Users\\taan\\Desktop\\Auto\\Drivers\\chrome\\chromedriver.exe")
+        cls.driver.implicitly_wait(10)
+        cls.driver.get("https://qa-dashboard.reverieinc.com/")
+        cls.driver.set_window_size(1920, 1080)
+    
+    def test_1_Login(self):
+        self.Login_To_Anuvadak()
+    
+    def test_2_Preview(self):
+        self.Preview_Loclization_via_Anuvadak()
+
+    @classmethod
+    def tearDown(cls):
+        print("Test Passed")
+        
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ Auto/Testcases/Page_Sync/test_4_Remove_content_P.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import unittest
+import time
+import random
+import string
+from Pages.Createprojectsyncpage1 import Createprojecsync1tmixin
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")
+
+
+class login(unittest.TestCase,Createprojecsync1tmixin):
+
+    translator_username = 'qatranslator@gmail.com'
+    translator_password = 'Rev@123'
+
+    randstring = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+   
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.driver = webdriver.Chrome(executable_path="C:\\Users\\taan\\Desktop\\Auto\\Drivers\\chrome\\chromedriver.exe", options=chrome_options)
+        #cls.driver = webdriver.Chrome(executable_path="C:\\Users\\taan\\Desktop\\Auto\\Drivers\\chrome\\chromedriver.exe")
+        cls.driver.implicitly_wait(10)
+        cls.driver.get("https://wordpress.com/pages/anuvadhaqa.wordpress.com")
+        cls.driver.set_window_size(1920, 1080)
+        
+    def test_1_Remove_Content(self):
+        self.Delete_Content()
+
+    @classmethod
+    def tearDown(cls):
+        print("Test Passed")
+        
+if __name__ == '__main__':
+    unittest.main()
