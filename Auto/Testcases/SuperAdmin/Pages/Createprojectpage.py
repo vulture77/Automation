a8ef75f50c6636c87b1b42d22590ba7f54af7595
vulture77 <60322348+vulture77@users.noreply.github.com>
2021-01-17 12:11:44 +0530

    Update Createprojectpage.py

--- Auto/Testcases/SuperAdmin/Pages/Createprojectpage.py
+++ Auto/Testcases/SuperAdmin/Pages/Createprojectpage.py
@@ -187,8 +187,7 @@ class Createprojectmixin:
         self.driver.find_element_by_name("baseUrl").send_keys("https://anuvadhaqa.wordpress.com/")
         self.driver.find_element_by_id("language").click()
         self.driver.find_element_by_xpath("//*[text()='hindi']").click()
-        time.sleep(2)
-        self.driver.find_element_by_xpath("//label[text()='Base URL']").click()
+        # self.driver.find_element_by_xpath("//label[text()='Base URL']").click()      // this is needed on Front End
         # self.driver.find_element_by_xpath("(//div[text()='Please select'])[2]").click()
         # self.driver.find_element_by_xpath("//li[text()='RealTime']").click()
         self.driver.find_element_by_xpath("//button[text()='Next']").click()
