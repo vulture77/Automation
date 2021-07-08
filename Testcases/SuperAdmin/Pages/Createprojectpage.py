95fecc5acf628b86d7ec30c09a21020ceaa889a8
vulture77 <60322348+vulture77@users.noreply.github.com>
2021-07-08 10:15:38 +0530

    Update Createprojectpage.py

--- Testcases/SuperAdmin/Pages/Createprojectpage.py
+++ Testcases/SuperAdmin/Pages/Createprojectpage.py
@@ -18,7 +18,7 @@ class Createprojectmixin:
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument('--ignore-certificate-errors')
     Path = "chrome/chromedriver.exe"
-    filepath = "D:\\a\\1\\schrome\\file.csv"
+    filepath = "D:\\a\\1\\s\\chrome\\file.csv"
     Anuvadak_Beta = "https://qa-dashboard.reverieinc.com/"
     Prabandhak_Beta = "http://beta.prabandhak.in/"
 
